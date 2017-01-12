c44cf331566ec6408b492eb280c51b67a1269af7
Dan Brown <slightlyoffbeat@gmail.com>
2017-01-12 01:01:38 -0800

    fixing redux devtools

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-underscore-dangle */
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers/index';
@@ -10,7 +10,8 @@ const defaultState = {
   },
 };
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
